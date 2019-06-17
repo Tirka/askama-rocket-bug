b420b4f79c0406b18eeb621a57089f1fa0a14c70
Maksim Vykhota <m.vykhota@protonmail.com>
2019-06-17 07:01:33 +0300

    init commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+#![cfg(feature = "with-rocket")]
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use askama::Template;
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
+#[get("/")]
+fn hello() -> HelloTemplate<'static> {
+    HelloTemplate { name: "world" }
+}
+
+fn main() {
+    let rocket = rocket::ignite().mount("/", routes![hello]).launch();
+}
